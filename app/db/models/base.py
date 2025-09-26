0e1892bfa02f9edae218f38a53013a38a1946f44
osha890 <oshavshav@gmail.com>
2025-09-26 13:51:45 +0300

    add created_at and updated_at fields to Base

--- app/db/models/base.py
+++ app/db/models/base.py
@@ -1,5 +1,7 @@
-from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, declared_attr
+from datetime import datetime
+
+from sqlalchemy import DateTime, MetaData, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 
 from core.config import settings
 from utils.case_converter import camel_case_to_snake_case
@@ -13,3 +15,16 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
